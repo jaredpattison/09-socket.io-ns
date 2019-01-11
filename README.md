e320c1a40825a38ba102d283526ec04b623c0703
jared <jaredpattison@users.noreply.github.com>
2019-01-11 14:04:05 -0800

    re-submit

--- README.md
+++ README.md
@@ -7,38 +7,22 @@
 
 ### Links and Resources
 * [repo](https://github.com/jaredpattison/09-socket.io-ns)
-* [travis](http://xyz.com)
-* [back-end](http://xyz.com)
-* [front-end](http://xyz.com)
+* [travis](https://www.travis-ci.com/jaredpattison/09-socket.io-ns)
+* [front-end](https://pmww0ww42q.codesandbox.io/)
 
 ### Modules
 #### `server.js`
 #### `app.js`
 #### `client.js`
-##### Exported Values and Methods
-
-###### `foo(thing) -> string`
-Usage Notes or examples
-
-###### `bar(array) -> array`
-Usage Notes or examples
-
-### Setup
-#### `.env` requirements
-* `PORT` - Port Number
-* `MONGODB_URI` - URL to the running mongo instance/db
 
 #### Running the app
-* `npm start`
-* Endpoint: `/foo/bar/`
-  * Returns a JSON object with abc in it.
-* Endpoint: `/bing/zing/`
-  * Returns a JSON object with xyz in it.
+* nodemon server.js to start server
+* node client.js to simulate client
+* node app.js to instigate timed emitts
 
 #### Tests
 * How do you run tests?
 * What assertions were made?
 * What assertions need to be / should be made?
 
-#### UML
-Link to an image of the UML for your application and response to events
+
--- server.js
+++ server.js
@@ -1,2 +1,57 @@
 'use strict';
 
+const io = require('socket.io')(3000);
+
+io.on('connection', (socket) => {
+  console.log(socket.id, 'connected');
+});
+
+// Numbers Namespace, Letters Namespace
+const numbers = io.of('/numbers');
+const letters = io.of('/letters');
+
+// Listeners for Numbers connections
+numbers.on('connection', (socket) => {
+  console.log('Connected to numbers', socket.id);
+
+  // connect to room inside of numbers
+  socket.on('join', (room) => {
+    socket.join(room);
+  });
+
+  // emmit positive to all in Numbers namespace
+  let i = 0;
+  socket.on('next-number', () => {
+    socket.broadcast.emit('number', i);
+   
+    // emmit negative for negative room
+    socket.in('negative').emit('_number', (-i));
+    i++;
+  });
+});
+
+// Listeners for Letters connections
+letters.on('connection', (socket) => { 
+  console.log('Connected to letters', socket.id);
+
+  //connect to room inside of letters
+  socket.on('join', (room) => {
+    socket.join(room);
+  });
+
+  // emmit letter
+  let i = 65;
+  socket.on('next-letter', () => {
+    socket.broadcast.emit('letter', String.fromCharCode(i));
+    // emmit lower case
+    let j = i + 32;
+    socket.in('lowercase').emit('_letter', String.fromCharCode(j));
+    i++;
+    if (i > 90) {
+      i = 65;
+    }
+  });
+});
+
+
+
